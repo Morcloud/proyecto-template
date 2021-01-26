852382d45ce836218c7d8af637d3295a0f00d05b
Ricardo Tornero <richardo@outlook.com>
2021-01-26 03:00:36 +0000

    index agregado

--- resources/views/admin/permiso/index.blade.php
+++ resources/views/admin/permiso/index.blade.php
@@ -6,5 +6,28 @@
 
 
 @section('contenido')
-    Permiso
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="box box-primary">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Permisos</h3>
+                </div>
+                <div class="box-body table-responsive no-padding">
+                    <table class="table table-bordered table-hover table-striped"> 
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Slug</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr></tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
